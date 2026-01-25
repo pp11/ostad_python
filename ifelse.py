87944d6218b3c52a0f2e915b6b191737cc7a9abf
Priyanka Podder (SIL) <priyanka@squaregroup.com>
2026-01-25 15:31:36 +0600

    first commit

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+num=  int(input('please enter a number '))
+print (num)
+
+if num>0:
+    print('this is positive number')
+elif num<0:
+    print('this is negetive number')
+else:
+    print('number is 0')
+
+
---
+++ string_indexing.py
@@ -0,0 +1,17 @@
+import math
+a='this is string'
+
+print(a.upper())
+print(a)
+
+f_name='test'
+l_name='test2'
+
+final="hello {f1_name} {l_name}.".format(f1_name=l_name,l_name=l_name)
+final2=f'hello {f_name} {l_name}.'
+print(final2)
+
+
+a=3.8 
+
+print(math.floor(a))
